9ee6a9bdd2fb1ee637366583aa562fbc48aac348
nahumoore <nahuelmoreno2904@gmail.com>
2021-02-10 17:55:21 -0300

    primer entrega

---
+++ README.txt
@@ -0,0 +1,5 @@
+Nahuel Moreno
+C.I.: 5373595-9
+
+
+Login template: https://colorlib.com/wp/template/login-form-v2/
---
+++ js/login.js
@@ -0,0 +1,17 @@
+let pass = document.getElementById("pass");
+let id = document.getElementById("id");
+
+function login(){
+if(id.value == ""){
+    alert("Ingresa tu ID")
+    return false;
+}else if(isNaN(id.value)){
+    alert("El id debe ser un numero")
+    return false;
+}else if(pass.value == ""){
+    alert("Ingresa tu contraseña")
+    return false;
+}else{
+    window.location.href = 'encomiendas.html';
+}
+}
